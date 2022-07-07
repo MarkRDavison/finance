3830b0c3b395a27d1f85eb910e12415c2c7fccc4
mark.davison <mark.davison@taxlab.online>
2022-07-07 12:46:45 +1200

    wip

--- src/entry.sh
+++ src/entry.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-echo 'entry.sh - starting - 1'
-cat ./index.html
-echo 'entry.sh - starting - 2'
-echo 'entry.sh - starting - 3'
-echo 'entry.sh - starting - 4'
 sed -i 's|window.ENV = {}|window.ENV = { FINANCE_BFF_BASE_URI: "'$FINANCE_BFF_BASE_URI'" }|g' ./index.html
 cat ./index.html
 nginx -g 'daemon off;'
