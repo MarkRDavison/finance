fd7bfd42526eddc5d2e8ea922fa7ecb6f5e49349
mark.davison <markdavison0@gmail.com>
2022-07-07 06:30:30 +0000

    Dev/bff user call localhost

--- src/common/mark.davison.finance.common.client/Authentication/IAuthenticationConfig.cs
+++ src/common/mark.davison.finance.common.client/Authentication/IAuthenticationConfig.cs
@@ -5,5 +5,6 @@ public interface IAuthenticationConfig
     string LoginEndpoint { get; set; }
     string LogoutEndpoint { get; set; }
     string UserEndpoint { get; set; }
+    string BffBase { get; }
     void SetBffBase(string bffBase);
 }
