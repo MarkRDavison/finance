35e728829bc8184d6a5b29a13fe81efdbb43505d
mark.davison <markdavison0+github@gmail.com>
2024-02-16 20:41:52 +1300

    update config

--- src/api/mark.davison.finance.api/Configuration/AppSettings.cs
+++ src/api/mark.davison.finance.api/Configuration/AppSettings.cs
@@ -4,7 +4,7 @@ public class AppSettings
 {
     public static string SECTION = "FINANCE";
 
-    public string AUTHORITY { get; set; } = "https://auth.markdavison.kiwi/auth/realms/markdavison.kiwi";
+    public string AUTHORITY { get; set; } = "https://keycloak.markdavison.kiwi/auth/realms/markdavison.kiwi";
     public string CLIENT_ID { get; set; } = "zeno-finance";
     public string CLIENT_SECRET { get; set; } = string.Empty;
     public string SESSION_NAME { get; set; } = "finance-session-name";
